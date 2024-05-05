b627b1d39e73d9b9d61031d82dd115b15ad7ce5a
almosrof120 <mosrof35-645@diu.edu.bd>
2024-05-05 10:46:02 +0600

    start ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TheMovieDB APK',
+      
       home: Scaffold(
-        body: AppBar(
-          backgroundColor: Colors.blue,
+       // backgroundColor: Colors.white70,
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: const Center(
+            child:  Text(
+              'TheMovieDB',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.amber,
+                fontSize: 20
+              ),),
+          ),
+          elevation: 0,
         ),
+        backgroundColor: const Color.fromARGB(129, 56, 222, 214),
+        body: Container(
+
+        ),
+        
+        
+        
+        
       ),
     );
     
