ccb3f097c49e946e45354e09d0d95bd21fcf8316
almosrof120 <mosrof35-645@diu.edu.bd>
2024-05-07 22:37:49 +0600

    splash screen updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,35 +4,3 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TheMovieDB APK',
-      
-      home: Scaffold(
-       // backgroundColor: Colors.white70,
-        appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
-          title: const Center(
-            child:  Text(
-              'TheMovieDB',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.amber,
-                fontSize: 20
-              ),),
-          ),
-          elevation: 0,
-        ),
-        backgroundColor: Colors.white,
-        body: Container(
-
-        ),
-      ),
-    );
-    
-  }
-}
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  final VoidCallback onInitializationComplete;
+
+
+  const SplashScreen ({
+  Key key, required this.onInitializationComplete,
+}): super(Key: key);
+
+
+
+
+  @override
+  State<StatefulWidget> createState() => _MyWidgetState();
+}
+
+
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
